b37b026b7bab9dff65c9e526c55ad0132bf27e20
Zachari Price <ZachariPrice3@gmail.com>
2023-05-02 14:10:41 -0400

    LINQ Exercise1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace LINQ
+﻿// using C34PracticeLinqExercise;
+
+
+
+List<string> xboxExclusiveGames = new List<string>() { "Halo Series", "Apex Legends", "NBA 2K Series" };
+
+xboxExclusiveGames.Add("Ninja Gaiden");
+xboxExclusiveGames.Add("Rocket League");
+
+var videoGames = xboxExclusiveGames.OrderByDescending(name => name.Length);
+
+foreach (var games in videoGames)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine(games);
 }
